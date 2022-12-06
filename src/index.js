f81f982e1b828cd247966c282f94b11e39190096
MannyDesktop <rhcplive11@gmail.com>
2022-12-06 13:19:44 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./style.css";
+
+const mainContent = () => {
+    const root = document.createElement('div');
+    root.classList.add('root');
+
+
+
+    return root;
+}
+
+document.body.appendChild(mainContent());
